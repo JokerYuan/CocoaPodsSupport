e962966fba1300505e5368db7b3b92ba59e6ae83
JokerYuan <1598004992@qq.com>
2017-06-30 11:36:24 +0800

    create

---
+++ CocoaPodsSupport/UIView+Frame.h
@@ -0,0 +1,35 @@
+//
+//  UIView+Frame.h
+//  CocoaPodsSupport
+//
+//  Created by joker on 2017/6/30.
+//  Copyright © 2017年 joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property (nonatomic, assign) CGFloat x;
+
+@property (nonatomic, assign) CGFloat y;
+
+@property (nonatomic, assign) CGFloat width;
+
+@property (nonatomic, assign) CGFloat height;
+
+@property (nonatomic, assign) CGFloat top;
+
+@property (nonatomic, assign) CGFloat left;
+
+@property (nonatomic, assign) CGFloat bottom;
+
+@property (nonatomic, assign) CGFloat right;
+
+@property (nonatomic, assign) CGSize size;
+
+
+@end
+
+
+
---
+++ CocoaPodsSupport/UIView+Frame.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Frame.m
+//  CocoaPodsSupport
+//
+//  Created by joker on 2017/6/30.
+//  Copyright © 2017年 joker. All rights reserved.
+//
+
+#import "UIView+Frame.h"
+
+@implementation UIView (Frame)
+
+@end
